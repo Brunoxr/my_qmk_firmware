360b145cb719f7b9b722442bda11daf9b2c42907
Brunoxr <Brunoxr@users.noreply.github.com>
2022-06-01 12:41:22 -0300

    Update keymap.c

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/default/keymap.c
@@ -9,11 +9,11 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_5x6(
-        KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSPC,
-        KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_MINS,
-        LSFT_T(KC_CAPS), KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                 KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
-        LSFT_T(KC_CAPS), KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                 KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH,
-        KC_LCTL,KC_RBRC,                                                       KC_PLUS, KC_EQL,
+        KC_ESC ,               KC_1    , KC_2    , KC_3             , KC_4           , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSPC,
+        KC_TAB ,               KC_Q    , KC_W    , KC_E             , KC_R           , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_MINS,
+        LSFT_T(OSM(MOD_LSFT)), KC_A    , KC_S    , KC_D             , KC_F           , KC_G  ,                 KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
+        LSFT_T(KC_CAPS),       KC_Z    , KC_X    , KC_C             , KC_V           , KC_B  ,                 KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH,
+        KC_LCTL,               KC_LGUI , KC_LALT , LT(_RAISE,KC_GRV), LCTL_T(KC_SPC)                                                     KC_PLUS, KC_EQL,
                                          RAISE,KC_SPC,                        KC_ENT, LOWER,
                                          KC_TAB,KC_HOME,                         KC_END,  KC_DEL,
                                          KC_BSPC, KC_GRV,                        KC_LGUI, KC_LALT
